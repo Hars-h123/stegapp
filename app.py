426995472684dd152efa851deba7c1fc36dad8ff
Harsh Damania <harshdamania2006@gmail.com>
2026-03-11 07:39:25 +0530

    fixed render deploymeny error.

--- app.py
+++ app.py
@@ -178,5 +178,8 @@ def download_file(filename):
 # =============================
 # Run server
 # =============================
+import os
+
 if __name__ == "__main__":
-    app.run(port=10000, debug=True)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
